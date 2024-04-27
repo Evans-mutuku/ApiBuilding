6e64102b6d82d1a0e95e26b07173a57785473f50
duke_maosa <dukemaosa75@gmail.com>
2024-04-27 16:31:56 -0400

    Intergrated mynewsAPI-plpPythonHackathon

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from django.shortcuts import render
+from .views import news_index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-  path('', views.index)
+    path('', news_index, name='news_index'),
+    
 ]
+
